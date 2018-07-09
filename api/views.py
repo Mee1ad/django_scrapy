7d22d597176126cb7290a5480994c541c0ebc844
Mee1ad <SoheilRavasani@Gmail.com>
2018-07-09 22:04:23 +0430

    make it better

--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from scrapyd_api import ScrapydAPI
 from json import JSONEncoder
 import timeit
+import json
 
 start = timeit.default_timer()
+scrapyd = ScrapydAPI('https://myscrapyd.herokuapp.com/')
 
 @csrf_exempt
 def api(request):
@@ -11,7 +14,19 @@ def api(request):
     end = timeit.default_timer()
     runtime = end - start
     return JsonResponse({
-        'status': 'ok',
+        'status': true,
         'text': "What do you expect to do?",
+        'time': runtime
+    }, encoder=JSONEncoder)
+    if request.method == 'POST':
+    	data = json.loads(request.body)
+    	state = data['state']
+    	movie = data['movie']
+    	category = data['category']
+    	job = scrapyd.schedule('crawler', 'search', category=f"{category}", movieName=f"{movie}")
+
+    	return JsonResponse({
+        'status': true,
+        'text': job,
         'time': runtime
     }, encoder=JSONEncoder)
