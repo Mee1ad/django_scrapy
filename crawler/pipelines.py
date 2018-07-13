f89570160985a5c8981a00413ccb6d9a73cf3b0e
Mee1ad <SoheilRavasani@Gmail.com>
2018-07-13 21:09:11 +0430

    make it better

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -5,10 +5,9 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+db = dataset.connect('postgresql://yraitcrdcotuhb:6c41e7f3055517601bffd7be8434801d2bb96234171828dc7f860432705b405d@ec2-107-20-224-137.compute-1.amazonaws.com:5432/d7fi5emvoirfqc')
 class CrawlerPipeline(object):
     def process_item(self, item, spider):
-        db = dataset.connect('postgres://yraitcrdcotuhb:6c41e7f3055517601bffd7be8434801d2bb96234171828dc7f860432705b405d@ec2-107-20-224-137.compute-1.amazonaws.com:5432/d7fi5emvoirfqc')
 		if item['category'] == 'movie':
 			table = db['movie']
 		if item['category'] == 'serial':
