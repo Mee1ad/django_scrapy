fef7ffc7052c26c55c941f56937ea856e3bb06e5
Mee1ad <SoheilRavasani@Gmail.com>
2018-07-15 11:29:10 +0430

    make it better

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api, name='api'),
     path(r'^api/search_movie/$', views.search_movie, name='search_movie'),
 ]
