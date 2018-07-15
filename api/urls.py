e09322cb8c693a9eb7cde03ae2928605283cbcd4
Mee1ad <SoheilRavasani@Gmail.com>
2018-07-15 11:23:51 +0430

    make it better

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.api, name='api'),
+    path(r'^api/search_movie/$', views.search_movie, name='search_movie'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,60 +7,47 @@ import json
 import dataset
 from stuf import stuf
 
-start = timeit.default_timer()
-scrapyd = ScrapydAPI('https://myscrapyd.herokuapp.com/')
+db = dataset.connect('postgresql://yraitcrdcotuhb:6c41e7f3055517601bffd7be8434801d2bb96234171828dc7f860432705b405d@ec2-107-20-224-137.compute-1.amazonaws.com:5432/d7fi5emvoirfqc')
 
 @csrf_exempt
-def api(request):
+def search_movie(request):
+	data = json.loads(request.body)
+	state = data['state']
+	name = data['name']
+	#name = '%' + name + '%'
+	table = db['movie']
+	sql = f"SELECT * FROM movie WHERE name LIKE '%{name}%'"
+	results = db.query(sql)
+	movies = []
+	movie = {}
+	for result in results:
+	    movie['name'] = result['name']
+	    movie['year'] = result['year']
+	    movie['photo'] = result['photo']
+	    movies.append(movie)
+	end = timeit.default_timer()
+	return JsonResponse({
+	'status': True,
+	'text': movies,
+	'time': end - start
+}, encoder=JSONEncoder)
 
-	if request.method == 'POST':
-		db = dataset.connect('postgresql://yraitcrdcotuhb:6c41e7f3055517601bffd7be8434801d2bb96234171828dc7f860432705b405d@ec2-107-20-224-137.compute-1.amazonaws.com:5432/d7fi5emvoirfqc')
-		data = json.loads(request.body)
-		state = data['state']
-		name = data['name']
-		#name = '%' + name + '%'
+@csrf_exempt
+def get_movie(request):
+	table = db['movie']
+	sql = f'SELECT * FROM movie WHERE name LIKE "{name}" LIMIT 10'
+	results = db.query(sql)
+	movie = {}
+	for result in results:
+	    movie['name'] = result['name']
+	    movie['year'] = result['year']
+	    movie['photo'] = result['photo']
+	    movie['link480'] = result['link480']
+	    movie['link720'] = result['link720']
+	    movie['link1080'] = result['link1080']
+	return JsonResponse({
+	'status': True,
+	'text': movie,
+	'time': end - start
+}, encoder=JSONEncoder)
 
-		if state == "search_movie":
-			table = db['movie']
-			sql = f"SELECT * FROM movie WHERE name LIKE '%{name}%'"
-			results = db.query(sql)
-			movies = []
-			movie = {}
-			for result in results:
-			    movie['name'] = result['name']
-			    movie['year'] = result['year']
-			    movie['photo'] = result['photo']
-			    movies.append(movie)
-			end = timeit.default_timer()
-			return JsonResponse({
-			'status': True,
-			'text': movies,
-			'time': end - start
-		}, encoder=JSONEncoder)
-
-		if state == "get_movie":
-			table = db['movie']
-			sql = f'SELECT * FROM movie WHERE name LIKE "{name}" LIMIT 1'
-			results = db.query(sql)
-			movie = {}
-			for result in results:
-			    movie['name'] = result['name']
-			    movie['year'] = result['year']
-			    movie['photo'] = result['photo']
-			    movie['link480'] = result['link480']
-			    movie['link720'] = result['link720']
-			    movie['link1080'] = result['link1080']
-			return JsonResponse({
-			'status': True,
-			'text': movie,
-			'time': end - start
-		}, encoder=JSONEncoder)
-
-
-	else:
-		end = timeit.default_timer()
-		return JsonResponse({
-		'status': "ok",
-		'text': "Method : post --- state = search_movie ---  name = 'Overboard' or 'Siberia' or other movies in our db :))",
-		'time': end - start
-	}, encoder=JSONEncoder)
